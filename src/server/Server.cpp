b1c392dabea696a1314480b96f89316cec2a95a4
Tal Ben Yehezkel <talbenyehezkel@gmail.com>
2022-02-06 10:00:37 +0200

    added info prints

--- src/server/Server.cpp
+++ src/server/Server.cpp
@@ -1,5 +1,6 @@
 
 #include "Server.h"
+#include <iostream>
 
 Server::Server()
     : _socket() {}
@@ -7,11 +8,14 @@ Server::Server()
 int Server::run(const char *ip, int port, int queue) {
     // todo, listen on a different thread, accept commands on this thread.
 
+    std::cout << "Binding server to host: " << ip << ", listenning on port " << port << std::endl;
     _socket.sbind(ip, port);
     _socket.slisten(queue);
+    std::cout << "Done." << std::endl;
 
     while (true) {
         sock::Socket client = _socket.saccept();
+        std::cout << "A new client has entered the chat, info: " << client << std::endl;
     }
 
     return 0;
